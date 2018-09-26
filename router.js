277cf5e4c173e9c928d181df799ef6026cc5d90b
Henry Trần <king@DarkMac.local>
2018-09-26 16:36:13 +0700

    update home test

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const api = require('./Controllers/api');
 
 function apply(app) {
     app.get("/", function (req, res) {
-        res.send("Home!");
+        res.send("Hello world!! I'm TnuApi on nodejs " + process.version);
     });
 
     app.get("/Hello_world", function (req, res) {
