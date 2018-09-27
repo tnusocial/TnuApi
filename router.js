ba2efb09ab9595e5e9bbf60be7e04db0ec4509ff
Henry Trần <king@DarkMac.local>
2018-09-27 16:01:21 +0700

    Update new body parse method!

--- router.js
+++ router.js
@@ -1,23 +1,9 @@
+const bodyParser = require('body-parser');
+
 const api = require('./Controllers/api');
 
 function registerDataProcess(app) {
-    app.use(function(req, res, next) {
-        req.rawBody = '';
-        req.setEncoding('utf8');
-
-        req.on('data', function(chunk) {
-            req.rawBody += chunk;
-        });
-
-        req.on('end', function() {
-            try {
-                req.body = JSON.parse(req.rawBody);
-            } catch (err) {
-
-            }
-            next();
-        });
-    });
+    app.use(bodyParser.json());
 }
 
 function apply(app) {
