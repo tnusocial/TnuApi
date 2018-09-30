5ab902030898f6a24a4574585d6226c72113d33c
Henry Trần <king@DarkMac.local>
2018-09-30 17:10:44 +0700

    update route table

--- router.js
+++ router.js
@@ -23,10 +23,6 @@ function apply(app) {
         res.send("Hello world!! I'm TnuApi on nodejs " + process.version);
     });
 
-    app.get("/Hello_world", function(req, res) {
-        res.send("Hello_world!");
-    });
-
     app.post("/login", api.login);
 }
 
