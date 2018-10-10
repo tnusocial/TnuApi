3132c37ed7ec4d07937191e1499256e053a54ad6
Henry Trần <king@DarkMac.local>
2018-10-10 23:00:59 +0700

    Update project struct. Implement socketio and webui.

--- router.js
+++ router.js
@@ -1,32 +1,10 @@
-const bodyParser = require('body-parser');
-
-const api = require('./Controllers/api');
-
-function registerDataProcess(app) {
-    app.use(function(req, res, next) {
-        req.rawBody = '';
-        req.setEncoding('utf8');
-
-        req.on('data', function(chunk) {
-            req.rawBody += chunk;
-        });
-
-        req.on('end', function() {
-            next();
-        });
-    });
-    app.use(bodyParser.json());
-}
-
-function apply(app) {
-    app.get("/", function(req, res) {
-        res.send("Hello world!! I'm TnuApi on nodejs " + process.version);
-    });
-
-    app.post("/login", api.login);
+function apply(app, socketio) {
+    require('./Routers/core').apply(app, socketio);
+    require('./Routers/webui').apply(app, socketio);
+    require('./Routers/api').apply(app, socketio);
+    require('./Routers/socket.io').apply(app, socketio);
 }
 
 module.exports = {
-    registerDataProcess: registerDataProcess,
     apply: apply
 };
--- run.js
+++ run.js
@@ -1,16 +1,22 @@
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const router = require('./router');
 
 // Load config
 const port =process.env.PORT||8080;
 
+// Init express
 const app = express();
+const server = http.Server(app);
+const sio = socketio(server);
 
-router.registerDataProcess(app);
+// Begin apply router table
+router.apply(app, sio);
+// End apple router table
 
-router.apply(app);
-
-console.log("TnuApi starting webservicer at port " + port);
-
-app.listen(port);
+server.listen(port, function () {
+    console.log("TnuApi starting webservicer at port " + port);
+    console.log("Listening...");
+});
